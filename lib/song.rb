40fcc6c4b7f4ee99b39e8d30ddae4058e3484701
rosa lee <rosalee0116@gmail.com>
2020-04-06 20:16:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,53 +12,7 @@ class Song
 
   def self.create
     song = self.new
-    song.save
     song
 end
 
-  def.self.new_by_name(name)
-    song = self.new
-    song.name = name
-    song
   end
-
-  def self.create_by_name(name)
-    song = self.create
-    song.name = name
-    song
-  end
-
-  def self.find_by_name(name)
-    @@all.each do |song|
-      if song.name == name
-        return name
-      end
-    end
-end
-
-  def self.find_or_create_by_name(name)
-    delf.find_by_name(name) || self.create_by_name(name)
-  end
-
-  def self. alphabetical
-    @@all.sort_by{|song| song.name}
-  end
-
-  def self.new_from_filename(filename)
-    row = filename
-    date = row.split(" - ")
-    artist_name = data[0]
-    song.name = data[1].gsub(".mp3", "")
-
-    song = self.new
-    song.name = song_name
-    song.artist_name = artist_name
-    song
-  end
-
-  def self.create_from_filename(filename)
-    song =self.new_from_filename(filename)
-    song.save
-    song
-  end
-end
